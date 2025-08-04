1d0c081bf2ee0b94f043bb579c48d5a72f1d43c8
muktimukashi <103751405+muktimukashi@users.noreply.github.com>
2025-08-04 10:49:33 +0700

    Update script.js
    Menambahkan javascript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function cekJawaban() {
+    let skor = 0;
 
+    let jawaban1 = document.getElementById("jawaban1").value;
+    if (jawaban1.toLowerCase() === "jakarta") {
+    skor++;
+    }
+
+    let jawaban2 = document.getElementById("jawaban2").value;
+    if (jawaban2 === "8") {
+    skor++;
+    }
+
+    document.getElementById("hasil").innerText = "Skor kamu: " + skor + "/10";
+}
